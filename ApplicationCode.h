f450a4cf2ce415b0406fdfb5846f47a61195b509
Melissa  Chavez <mech6371@colorado.edu>
2024-09-05 12:50:10 -0600

    Update ApplicationCode.h

--- ApplicationCode.h
+++ ApplicationCode.h
@@ -3,8 +3,7 @@
  *
  *  Created on: Aug 27, 2024
  *      Author: Owner
- */
-
+//Header files are needed to declare functions and variables that are available. You might not have access to the definitions (=the .c files) at all; C supports binary-only distribution of code in libraries
 
 #ifndef APPLICATIONCODE_H_
 #define APPLICATIONCODE_H_
